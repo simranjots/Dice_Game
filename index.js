fefdf094ffb7d65fb080744497970ee74a626f27
simranjot singh <32786222+simranjots@users.noreply.github.com>
2022-05-30 19:13:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".dice .img1").setAttribute("src","images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".dice .img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector(".container h1").innerHTML = "🚩 Player 1 Wins!";
+}else if (randomNumber2 > randomNumber1){
+  document.querySelector(".container h1").innerHTML =  "Player 2 Wins! 🚩";
+}else{
+  document.querySelector(".container h1").innerHTML = "Draw!";
+}
